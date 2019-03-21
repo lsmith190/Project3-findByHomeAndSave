1f8f547ae199f41db9bdacc79b3c6129f02eb79b
lsmith190 <lindseyr.smith1217@gmail.com>
2019-03-21 17:10:55 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(__dirname + '/client/build/'));
 app.get('/', (req, res) => {
     res.send('Hello world!')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
 })
 
 const PORT = process.env.PORT || 3001;
