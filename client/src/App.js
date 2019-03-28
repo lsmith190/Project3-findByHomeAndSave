08970f4a4ea2a4979ce4aa6a25477083a855614d
lsmith190 <lindseyr.smith1217@gmail.com>
2019-03-28 01:16:32 -0400

    created route for new users

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './components/HomePage.js'
 import AllHomes from './components/AllHomes.js'
 import NewHome from './components/NewHome.js'
 import EditHome from './components/EditHome.js'
+import NewUser from './components/NewUser.js'
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage}/>
+          <Route exact path="new" component={NewUser}/>
           <Route exact path="/user/:userId/homes" component={AllHomes} />
           <Route exact path="/user/:userId/homes/new" component={NewHome} />
           <Route exact path="/user/:userId/homes/:homeId/edit" component={EditHome} />
